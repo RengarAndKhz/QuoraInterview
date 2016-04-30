b4796c55bad2906e602434ff32806c03702a39f7
Hedi Wang <wanghedi1991@gmail.com>
2016-04-30 12:11:53 -0400

    version 4.30.12.11

--- Permutation.py
+++ Permutation.py
@@ -4,3 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        result = []
+        self.helper(nums, [], result)
+        return result
+
+    def helper(self, nums, temp, result):
+        if len(nums) == 0:
+            result.append(temp)
+        for i in xrange(len(nums)):
+            self.helper(nums[:i] + nums[i+1:], temp+[nums[i]], result)
+
+testCase = Solution()
+print(testCase.permute([1,2,3]))
